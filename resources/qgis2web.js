9aa577b12c6aaeacc62c25724eda124a97da6075
Civitas-cuu <silvag.arq@gmail.com>
2026-09-15 13:11:29 -0600

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -184,10 +184,16 @@ function createPopupField(currentFeature, currentFeatureKeys, layer) {
             if (layer.get('fieldLabels')[currentFeatureKeys[i]] == "header label - always visible" ||
                 layer.get('fieldLabels')[currentFeatureKeys[i]] == "header label - visible with data") {
                 popupField += '<strong>' + layer.get('fieldAliases')[currentFeatureKeys[i]] + '</strong><br />';
-            }
-            if (layer.get('fieldImages')[currentFeatureKeys[i]] != "ExternalResource") {
-				popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? autolinker.link(currentFeature.get(currentFeatureKeys[i]).toLocaleString()) + '</td>' : '');
-			} else {
+if (currentFeatureKeys[i] === 'PDF') {
+    var pdf = currentFeature.get('PDF');
+    popupField += (pdf != null && pdf !== ''
+        ? '<a href="' + pdf + '" target="_blank" style="display:inline-block;padding:6px 12px;background:#2E7D32;color:#fff;text-decoration:none;border-radius:4px;font-weight:bold;"> Abrir plano</a>'
+        : 'Sin archivo');
+} else if (layer.get('fieldImages')[currentFeatureKeys[i]] != "ExternalResource") {
+    popupField += (currentFeature.get(currentFeatureKeys[i]) != null
+        ? autolinker.link(currentFeature.get(currentFeatureKeys[i]).toLocaleString())
+        : '');
+} else {
 				var fieldValue = currentFeature.get(currentFeatureKeys[i]);
 				if (/\.(gif|jpg|jpeg|tif|tiff|png|avif|webp|svg)$/i.test(fieldValue)) {
 					popupField += (fieldValue != null ? '<img src="images/' + fieldValue.replace(/[\\\/:]/g, '_').trim() + '" /></td>' : '');
